f0f3187e05fd3e28b45cb409036753bd25e307e0
Martin Gandon <martin.gandon@free.fr>
2024-05-06 16:38:10 +0200

    adding config for airbyte but error in integrating airbyte

--- airflow/dags/elt_dag.py
+++ airflow/dags/elt_dag.py
@@ -1,12 +1,15 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from airflow import DAG
 from docker.types import Mount
+from airflow.utils.dates import days_ago
 
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.bash import BashOperator
-
+# Module to trigger sync connections
+from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
 from airflow.providers.docker.operators.docker import DockerOperator
 import subprocess
+
+# Replace this string with the ID generated from your Airbyte instance
+CONN_ID = ""
 
 default_args = {
     "owner": "airflow",
@@ -14,16 +17,6 @@ default_args = {
     "email_on_failure": False,
     "email_on_retry": False,
 }
-
-
-def run_elt_script():
-    script_path = "/opt/airflow/elt/elt_script.py"
-    result = subprocess.run(["python", script_path], capture_output=True, text=True)
-    if result.returncode != 0:
-        raise Exception(f"Script failed with error: {result.stderr}")
-    else:
-        print(result.stdout)
-
 
 dag = DAG(
     "elt_and_dbt",
@@ -33,9 +26,13 @@ dag = DAG(
     catchup=False,
 )
 
-t1 = PythonOperator(
-    task_id="run_elt_script",
-    python_callable=run_elt_script,
+t1 = AirbyteTriggerSyncOperator(
+    task_id="airbyte_postgres_postgres",
+    airbyte_conn_id="airbyte",
+    connection_id=CONN_ID,
+    asynchronous=False,
+    timeout=3600,
+    wait_seconds=3,
     dag=dag,
 )
 
---
+++ elt.sh
@@ -0,0 +1,6 @@
+docker compose up init-airflow
+
+sleep 5
+
+docker compose up -d    
+
